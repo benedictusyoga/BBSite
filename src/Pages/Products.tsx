b78d637ffa6b81d514ada02a77fee96b029ee7d4
Benedictus Yogatama <yogatama4561@gmail.com>
2024-11-18 16:55:11 +0700

    product info fix

--- src/Pages/Products.tsx
+++ src/Pages/Products.tsx
@@ -4,12 +4,12 @@ import "./Products.css";
 import Elipse6 from "../assets/Ellipse6.svg";
 import Elipse5 from "../assets/Ellipse5.svg";
 // import Ruby1 from "../assets/rubyS.png";
-import Cobalt from "../assets/Cobalt.png"
-import Copper from "../assets/Copper.png"
-import Ruby from "../assets/Ruby.png"
-import copperP from "../assets/copperP.png"
-import navyP from "../assets/navyP.png"
-import redP from "../assets/redP.png"
+import Cobalt from "../assets/Cobalt.png";
+import Copper from "../assets/Copper.png";
+import Ruby from "../assets/Ruby.png";
+import copperP from "../assets/copperP.png";
+import navyP from "../assets/navyP.png";
+import redP from "../assets/redP.png";
 export default function Products() {
   const [scrollPosition, setScrollPosition] = useState(0);
   useEffect(() => {
@@ -44,9 +44,20 @@ export default function Products() {
         <div className="small-text">
           <div className="left-wrapper">
             <h1 className="large-text">Classic</h1>
-            <h4 className="left-text">The standard issue batik billiard glove made for comfort, durability, and elegance</h4>
+            <h4 className="left-text">
+              The standard issue batik billiard glove made for comfort,
+              durability, and elegance
+            </h4>
           </div>
-          <h4 className="right-text">Includes billiard glove with sleek batik designs, being Cobalt, Ruby, and Copper </h4>
+          <h4 className="right-text">
+            Billiard Glove but with Added <b>Durability</b>, <b>Comfort</b>, and{" "}
+            <b>Elegance</b>!
+            <br />
+            <br />
+            <b>MATERIAL</b>:
+            <br />- <b>100% </b>Stretchy and Durable <b>Spandex</b> <br />-
+            Lycra Batik Fabric
+          </h4>
         </div>
       </div>
       <img
@@ -64,14 +75,27 @@ export default function Products() {
         }}
       />
       <div className="carousel-centering">
-
         <Carousel slides={slides1} />
         <div className="small-text">
           <div className="left-wrapper">
             <h1 className="large-text">Classic+</h1>
-            <h4 className="left-text">The billiard gloves for those who want more. Comfort, durability, and elegance shines brighter with its additional features</h4>
+            <h4 className="left-text">
+              The billiard gloves for those who want more. Comfort, durability,
+              and elegance shines brighter with its additional features
+            </h4>
           </div>
-          <h4 className="right-text">Comes with velcro strap for securing the wrist and anti slip material for optimal performance. Designs include Cobalt, Ruby, and Copper</h4>
+          <h4 className="right-text">
+            <b>MATERIAL</b>:
+            <br />- <b>100% </b>Stretchy and Durable <b>Spandex</b> <br />-
+            Lycra Batik Fabric
+            <br />
+            <br />
+            <b>ADDITIONAL FEATURES</b>:
+            <br />
+            - <b>Anti-Slip</b> Material
+            <br />
+            - Auto-fitting <b>Velcro Strap</b>
+          </h4>
         </div>
       </div>
     </div>
